11fc2b154889334fad34c781c222835c3ea8fe53
Prudhvi Gadiraju <gadiraju.prudhvi@gmail.com>
2019-05-20 20:59:49 -0700

    Setup Circles

--- Untangler/ViewController.swift
+++ Untangler/ViewController.swift
@@ -10,9 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var currentLevel = 0
+    var connections = [UIView]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        levelUp()
+    }
+    
+    func levelUp() {
+        currentLevel += 1
+        
+        connections.forEach { $0.removeFromSuperview() }
+        connections.removeAll()
+        
+        for _ in 1...(currentLevel+4) {
+            let connection = UIView(frame: CGRect(origin: .zero, size: CGSize(width: 44, height: 44)))
+            connection.backgroundColor = .white
+            connection.layer.cornerRadius = 22
+            connection.layer.borderWidth = 2
+            connection.layer.borderColor = UIColor.black.cgColor
+            connections.append(connection)
+            view.addSubview(connection)
+        }
     }
 
 
