635ed0cf83bef26fd98a11a3f350c99512f6141a
Prudhvi Gadiraju <gadiraju.prudhvi@gmail.com>
2019-05-20 21:13:41 -0700

    ConnectionView

---
+++ Untangler/ConnectionView.swift
@@ -0,0 +1,54 @@
+//
+//  ConnectionView.swift
+//  Untangler
+//
+//  Created by Prudhvi Gadiraju on 5/20/19.
+//  Copyright © 2019 Prudhvi Gadiraju. All rights reserved.
+//
+
+import UIKit
+
+class ConnectionView: UIView {
+    
+    var dragChanged: (() -> ())?
+    var dragFinished: (() -> ())?
+    
+    var touchStartPos = CGPoint.zero
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touch = touches.first else { return }
+        touchStartPos = touch.location(in: self)
+        
+        touchStartPos.x -= frame.width / 2
+        touchStartPos.y -= frame.height / 2
+        
+        transform = CGAffineTransform(scaleX: 1.15, y: 1.15)
+        superview?.bringSubviewToFront(self)
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touch = touches.first else { return }
+        let point = touch.location(in: superview)
+        
+        center = CGPoint(x: point.x - touchStartPos.x, y: point.y - touchStartPos.y)
+        dragChanged?()
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        transform = .identity
+        dragFinished?()
+    }
+    
+    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+        touchesEnded(touches, with: event)
+    }
+}
--- Untangler/ViewController.swift
+++ Untangler/ViewController.swift
@@ -11,7 +11,7 @@ import UIKit
 class ViewController: UIViewController {
 
     var currentLevel = 0
-    var connections = [UIView]()
+    var connections = [ConnectionView]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,7 +26,7 @@ class ViewController: UIViewController {
         connections.removeAll()
         
         for _ in 1...(currentLevel+4) {
-            let connection = UIView(frame: CGRect(origin: .zero, size: CGSize(width: 44, height: 44)))
+            let connection = ConnectionView(frame: CGRect(origin: .zero, size: CGSize(width: 44, height: 44)))
             connection.backgroundColor = .white
             connection.layer.cornerRadius = 22
             connection.layer.borderWidth = 2
@@ -34,6 +34,14 @@ class ViewController: UIViewController {
             connections.append(connection)
             view.addSubview(connection)
         }
+        
+        connections.forEach(place)
+    }
+    
+    func place(_ connection: ConnectionView) {
+        let randomX = CGFloat.random(in: 20...view.bounds.maxX - 20)
+        let randomY = CGFloat.random(in: 50...view.bounds.maxY - 50)
+        connection.center = CGPoint(x: randomX, y: randomY)
     }
 
 
