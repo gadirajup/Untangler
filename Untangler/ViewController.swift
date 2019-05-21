cdd45c72f3ba1f2e8238252d8e966ec37f930190
Prudhvi Gadiraju <gadiraju.prudhvi@gmail.com>
2019-05-20 21:47:23 -0700

    Finish

--- Untangler/ViewController.swift
+++ Untangler/ViewController.swift
@@ -15,12 +15,32 @@ class ViewController: UIViewController {
     
     let renderedLines = UIImageView()
     
+    var scoreLabel = UILabel()
+    
+    var score = 0 {
+        didSet {
+            scoreLabel.text = "\(score)"
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        view.backgroundColor = .darkGray
+        
+        score = 0
+        scoreLabel.textColor = .cyan
+        scoreLabel.font = UIFont.preferredFont(forTextStyle: .headline)
+        scoreLabel.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(scoreLabel)
+        NSLayoutConstraint.activate([
+            scoreLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            scoreLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 32),
+        ])
+        
+        
         setupImageView()
         levelUp()
-        redrawLines()
     }
     
     func setupImageView() {
@@ -53,6 +73,10 @@ class ViewController: UIViewController {
             connection.dragChanged = { [weak self] in
                 self?.redrawLines()
             }
+            
+            connection.dragFinished = { [weak self] in
+                self?.checkMove()
+            }
         }
         
         for i in 0 ..< connections.count {
@@ -63,7 +87,11 @@ class ViewController: UIViewController {
             }
         }
         
-        connections.forEach(place)
+        repeat {
+            connections.forEach(place)
+        } while levelClear()
+        
+        redrawLines()
     }
     
     func place(_ connection: ConnectionView) {
@@ -77,11 +105,90 @@ class ViewController: UIViewController {
         
         renderedLines.image = renderer.image { ctx in
             for connection in connections {
-                UIColor.green.set()
+                var isLineClear = true
+                
+                for other in connections {
+                    if linesCross(start1: connection.center, end1: connection.after.center, start2: other.center, end2: other.after.center) != nil {
+                        isLineClear = false
+                        break
+                    }
+                }
+                
+                if isLineClear {
+                    UIColor.green.set()
+                } else {
+                    UIColor.red.set()
+                }
+                
                 ctx.cgContext.strokeLineSegments(between: [connection.after.center, connection.center])
             }
         }
     }
-
+    
+    func linesCross(start1: CGPoint, end1: CGPoint, start2: CGPoint, end2: CGPoint) -> (x: CGFloat, y: CGFloat)? {
+        // calculate the differences between the start and end X/Y positions for each of our points
+        let delta1x = end1.x - start1.x
+        let delta1y = end1.y - start1.y
+        let delta2x = end2.x - start2.x
+        let delta2y = end2.y - start2.y
+        
+        // create a 2D matrix from our vectors and calculate the determinant
+        let determinant = delta1x * delta2y - delta2x * delta1y
+        
+        if abs(determinant) < 0.0001 {
+            // if the determinant is effectively zero then the lines are parallel/colinear
+            return nil
+        }
+        
+        // if the coefficients both lie between 0 and 1 then we have an intersection
+        let ab = ((start1.y - start2.y) * delta2x - (start1.x - start2.x) * delta2y) / determinant
+        
+        if ab > 0 && ab < 1 {
+            let cd = ((start1.y - start2.y) * delta1x - (start1.x - start2.x) * delta1y) / determinant
+            
+            if cd > 0 && cd < 1 {
+                // lines cross – figure out exactly where and return it
+                let intersectX = start1.x + ab * delta1x
+                let intersectY = start1.y + ab * delta1y
+                return (intersectX, intersectY)
+            }
+        }
+        
+        // lines don't cross
+        return nil
+    }
+    
+    func levelClear() -> Bool {
+        for connection in connections {
+            for other in connections {
+                if linesCross(start1: connection.center, end1: connection.after.center, start2: other.center, end2: other.after.center) != nil {
+                    return false
+                }
+            }
+        }
+        return true
+    }
+    
+    func checkMove() {
+        if levelClear() {
+            score += currentLevel * 2
+            
+            view.isUserInteractionEnabled = false
+            UIView.animate(withDuration: 0.5, delay: 1, options: [], animations: {
+                self.renderedLines.alpha = 0
+                
+                for connection in self.connections {
+                    connection.alpha = 0
+                }
+                
+            }) { (finished) in
+                self.view.isUserInteractionEnabled = true
+                self.renderedLines.alpha = 1
+                self.levelUp()
+            }
+        } else {
+            score -= 1
+        }
+    }
 }
 
